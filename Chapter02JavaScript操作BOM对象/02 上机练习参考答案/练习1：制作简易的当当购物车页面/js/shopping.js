3a91af639af3c115ced62c22eba358c67ad24f6b
王涛 <desl0451@126.com>
2017-04-07 16:06:38 +0800

    第二章示例2完成

---
+++ Chapter02JavaScript操作BOM对象/02 上机练习参考答案/练习1：制作简易的当当购物车页面/js/shopping.js
@@ -0,0 +1,25 @@
+/**
+ * Created by 王涛 on 2017-04-07.
+ */
+function close_plan() {
+    window.close();
+}
+
+function collection() {
+    var flag = confirm("移入收藏后,将不在购物车显示,是否继续操作?");
+    if (flag == true) {
+        alert("移入收藏成功!");
+    }
+}
+function del() {
+    var flag = confirm("确定要删除吗?");
+    if (flag == true) {
+        alert("删除成功!");
+    }
+}
+function accounts() {
+    var flag = confirm("您本次购买的商品信息如下：\n\n商品名称：白岩松：白说、岛上书店；\n商品数量：2件；\n商品总计：46.00；\n运费：0元；\n\n请确认以上信息是否有误！！！");
+    if (flag) {
+        alert("您的订单已提交");
+    }
+}
